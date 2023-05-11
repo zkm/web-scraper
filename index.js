35497256839850632f1cae236cc0384d4485f5d9
Zach Schneider <me@zachschneider.com>
2023-05-11 15:16:01 -0500

    refresh

--- index.js
+++ index.js
@@ -1,29 +1,38 @@
 const PORT = 8000;
 const axios = require("axios");
 const cheerio = require("cheerio");
-const { response } = require("express");
 const express = require("express");
 
 const app = express();
 
 const url = "https://www.aol.com/news";
 
-axios(url)
-  .then((response) => {
-    const html = response.data;
-    const $ = cheerio.load(html);
-    const articles = [];
+async function scrapeArticles(url) {
+  const response = await axios(url);
+  const html = response.data;
+  const $ = cheerio.load(html);
+  const articles = [];
 
-    $(`.ntk-stream-item`, html).each(function () {
-      const title = $(this).find(".ntk-list-item-title").text();
-      const url = $(this).attr("href");
-      articles.push({
-        title,
-        url,
-      });
+  $(".ntk-stream-item", html).each(function () {
+    const title = $(this).find(".ntk-list-item-title").text();
+    const url = $(this).attr("href");
+    articles.push({
+      title,
+      url,
     });
-    console.log(articles);
-  })
-  .catch((err) => console.log(err));
+  });
+
+  return articles;
+}
+
+app.get("/articles", async (req, res) => {
+  try {
+    const articles = await scrapeArticles(url);
+    res.json(articles);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal server error");
+  }
+});
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
